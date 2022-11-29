9dcf2265b5afa3f2431ce525840c81249a37651e
Jack1ee7 <nikita.chukanov@gmail.com>
2022-11-29 18:00:08 +0300

    fix: 3rd review

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,24 @@
 const jwt = require('jsonwebtoken');
-
 const AuthError = require('../utils/errors/AuthError');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-const extractBearerToken = (header) => header.replace('Bearer ', '');
-
 module.exports = (req, res, next) => {
-  const authorization = req.cookies.jwt;
+  const token = req.cookies.jwt;
 
-  if (!authorization) {
-    throw new AuthError('Необходима авторизация');
+  if (!token) {
+    return next(new AuthError('Необходима авторизация'));
   }
 
-  const token = extractBearerToken(authorization);
   let payload;
 
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new AuthError('Необходима авторизация');
+    next(new AuthError('Необходима авторизация'));
   }
+
   req.user = payload; // записываем пейлоуд в объект запроса
 
-  next(); // пропускаем запрос дальше
+  return next(); // пропускаем запрос дальше
 };
